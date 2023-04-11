d7aa2bb13a2705dced50e76606caf4837561cc26
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2023-04-11 23:12:05 +0200

    Automate package publishing to pypi

--- pyhfs/__init__.py
+++ pyhfs/__init__.py
@@ -3,5 +3,3 @@ from .session import Session
 from .client import Client
 from .client import ClientSession
 from .exception import *  # Automatically imports all public exception
-
-__version__ = "0.0.1"
